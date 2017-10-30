a4a95dffb90c33af5cafe0642dcea3dd3795e095
sjli <2016226040006@stu.scu.edu.cn>
2017-10-30 19:07:29 +0800

    Update main.cpp

--- 20.pop and push stack/main.cpp
+++ 20.pop and push stack/main.cpp
@@ -9,31 +9,45 @@ bool IsPopOrder(vector<int> pushV, vector<int> popV)
 	if (popV.empty() && pushV.empty())
 		return true;
 
-	stack<int> s0;
-	int i = 1;
+	stack<int> s;
+	int i = 0;
 	int j = 0;
-	s0.push(pushV[0]);
-
-	while(i<pushV.size())
+	bool res;
+	while (i < pushV.size())
 	{
-		while (s0.top() != popV[j])
+		s.push(pushV[i++]);
+		while (!s.empty() && s.top() == popV[j])
 		{
-			s0.push(pushV[i++]);
+			s.pop();
+			j++;
 		}
-		if (i == pushV.size())
-			return false;
-
-		if (s0.top() == popV[j++])
-			s0.pop();
 	}
+	return s.empty();
 
-	return true;
+
+	//if (pushV.size() == 0)
+	//	return false;
+	//vector<int> s;
+	//for (int i = 0, j = 0; i < pushV.size();)
+	//{
+	//	s.push_back(pushV[i++]);
+	//	while (j < popV.size() && !s.empty() && s.back() == popV[j])
+	//	{
+	//		s.pop_back();
+	//		j++;
+	//	}
+	//}
+	//return s.empty();
 }
 
 int main()
 {
-	vector<int> pushV = { 1, 2, 3, 4, 5 };
-	vector<int> popV = { 4, 5, 1, 2, 3 };
+	vector<int> pushV = { 1 ,2,3,4,5};
+	//vector<int> popV = { 4, 5, 1, 2, 3 };
+	vector<int> popV = { 1,2,3,4,5 };
+	//vector<int> popV = { 4, 3, 5, 1, 2 };
+
+	//vector<int> popV = { 4,1,2};
 
 	if (IsPopOrder(pushV, popV))
 		cout << "Yes" << endl;
