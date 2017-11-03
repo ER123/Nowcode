d6dc8cc93222b898c17b12c9b84ee6a567971403
sjli <2016226040006@stu.scu.edu.cn>
2017-11-03 17:47:42 +0800

    Update main.cpp

--- 22.VerifySquenceOfBST/main.cpp
+++ 22.VerifySquenceOfBST/main.cpp
@@ -11,13 +11,13 @@ bool isSBT(vector<int> seq, int len)
 	if (seq.empty() || len <= 0)
 		return false;
 	int i = 0;
-	for (; i < len-1;i++)
+	for (; i < len-1;i++)//找到最后一个小于根节点的
 	{
 		if (seq[i] > seq[len-1])
 			break;
 	}
 	int j = i;
-	for (; j < len-1; j++)
+	for (; j < len-1; j++)//最后一个小于根节点后面的结点都不小于根节点，否则就不是二叉搜索树
 	{
 		if (seq[j]<seq[len-1])
 			return false;
@@ -25,12 +25,12 @@ bool isSBT(vector<int> seq, int len)
 
 	bool left = true;
 	if (i > 0)
-		left = isSBT(seq, i);
+		left = isSBT(seq, i);//验证左半部分
 	bool right = true;
 	if (i < len - 1)
 	{
 		seq = adjust(seq, i);
-		right = isSBT(seq, len - i - 1);
+		right = isSBT(seq, len - i - 1);//验证右半部分
 	}
 
 	return (left&&right);
