c112024afa10ccf2dbd1b3c3243f007a962e6bf8
sjli <2016226040006@stu.scu.edu.cn>
2018-08-10 20:42:47 +0800

    Update main.cpp

--- dp/main.cpp
+++ dp/main.cpp
@@ -44,7 +44,7 @@ int n = 3, m = 7;
 int A[4][8] = { { 0, 0, 0, 0, 0, 0, 0, 0 }, { 0, 1, 2, 4, 3, 5, 2, 4 }, { 0, 2, 5, 1, 2, 5, 2, 2 }, { 0, 4, 4, 3, 5, 1, 2, 3 } };
 int M[4][8];//记录每个坐标得到最多的苹果数
 
-int apple(int x, int y)
+int apple()
 {
 	for (int i = 1; i <= n; i++)
 	{
@@ -53,24 +53,34 @@ int apple(int x, int y)
 			if (i == 1 && j == 1)
 			{
 				M[1][1] = A[1][1];
+				mycoord[i][j].x = i;
+				mycoord[i][j].y = j;
 			}
 			if (i == 1 && j != 1)
 			{
 				M[i][j] = M[i][j - 1] + A[i][j];
+				mycoord[i][j].x = i;
+				mycoord[i][j].y = j-1;
 			}
-			if (x != 1 && y == 1)
+			if (i != 1 && j == 1)
 			{
 				M[i][j] = M[i - 1][j] + A[i][j];
+				mycoord[i][j].x = i - 1;
+				mycoord[i][j].y = j;
 			}
 			else if (i!=1 && j!=1)
 			{
 				if (A[i][j - 1] > A[i - 1][j])
 				{
 					M[i][j] = M[i][j-1] + A[i][j];
+					mycoord[i][j].x = i;
+					mycoord[i][j].y = j - 1;
 				}
 				else
 				{
 					M[i][j] = M[i - 1][ j] + A[i][j];
+					mycoord[i][j].x = i - 1;
+					mycoord[i][j].y = j;
 				}
 			}
 		}
@@ -83,20 +93,18 @@ int main()
 	//int min = findMinCoin(15);
 	//cout << min << endl;
 
-	apple(1, 2);
-	for (int i = 1; i <= 2; i++)
+	apple();
+	int n = 3, m = 3;
+	int nn, mm;
+	while (n != 1 || m != 1)
 	{
-		for (int j = 1; j <= 3; j++)
-		{
-			cout << M[i][j] << " ";
-		}
-		cout << endl;
+		cout << n << " " << m << endl;
+		nn = n;
+		mm = m;
+		n = mycoord[nn][mm].x;
+		m = mycoord[nn][mm].y;
 	}
-	for (int i = 1; i <= 5; i++)
-	{
-		cout << mycoord[i].x << " " << mycoord[i].y << endl;
-	}
-
+	cout << "1 1" << endl;
 	system("pause");
 	return 0;
 }
